b85e7ebd39580388b0d0418607296f1c41f9bfec
Tibet Tornaci <oofdere@hey.com>
2024-08-02 21:40:46 -0700

    make it a plugin

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
-import { transform } from 'lightningcss';
+import { composeVisitors, transform, type CustomAtRules, type Visitor } from 'lightningcss';
+
+export const twUnit: Visitor<CustomAtRules> = {
+    Token: {
+        dimension(token) {
+            if (token.unit === 'tw') {
+                return {
+                    raw: `${token.value * 0.25}rem`
+                }
+            }
+        }
+    }
+} as const;
 
 let { code, map } = transform({
     filename: 'test.css',
     minify: true,
     code: new TextEncoder().encode('.foo {size: 0.25tw}'),
-    visitor: {
-        Token: {
-            dimension(token) {
-                if (token.unit === 'tw') {
-                    return {
-                        raw: `${token.value * 0.25}rem`
-                    }
-                }
-            }
-        }
-    }
+    visitor: composeVisitors([twUnit])
 });
 
 console.log(code.toString())
