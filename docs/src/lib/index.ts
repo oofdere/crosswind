25f2d67faaa0bbf27dd6da8ff28873d6fd81d3d1
Tibet Tornaci <oofdere@hey.com>
2024-08-09 21:37:58 -0700

    [repl] codejar and syntax highlighting
    my head hurts

--- docs/src/lib/index.ts
+++ docs/src/lib/index.ts
@@ -1,1 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { createHighlighter } from "shiki";
+
+export const highlighter = await createHighlighter({
+    langs: ['css'],
+    themes: ['github-dark']
+});
+
+export const codeToHtml = (c: string) => {
+    return highlighter.codeToHtml(c, {
+        lang: 'css',
+        theme: 'github-dark'
+    })
+}
--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,15 +1,30 @@
-<script lang="ts">
+<script lang="ts" runes="{true}">
 	import '../app.css';
 	import demo from '$lib/demo.css.txt?raw';
 	import { createTransform } from '$lib/transform.svelte';
+	import { CodeJar } from 'codejar';
+	import { onMount } from 'svelte';
+	import { highlighter, codeToHtml } from '$lib';
 
 	const t = createTransform(demo);
+
+	let e: HTMLElement;
+	onMount(() => {
+		const jar = new CodeJar(e, (editor) => {
+			editor.innerHTML = codeToHtml(editor.textContent);
+		});
+		jar.updateCode(t.input);
+		jar.onUpdate(() => {
+			t.input = jar.toString();
+			console.log(t.input);
+		});
+	});
+
+	const html = $derived(codeToHtml(t.code));
 </script>
 
 <div class="flex w-screen h-screen flex-col-reverse md:flex-row">
-	<div class="h-full w-full">
-		<textarea class="h-full w-full" bind:value={t.input}></textarea>
-	</div>
+	<div class="h-full w-full" bind:this={e}></div>
 	<div class="h-full w-full flex flex-col">
 		{#if t.error}
 			<div class="bg-red-700 text-white">
@@ -17,7 +32,6 @@
 				<hr />
 			</div>
 		{/if}
-
-		<textarea class="h-full w-full" bind:value={t.code} disabled></textarea>
+		<div>{@html html}</div>
 	</div>
 </div>
